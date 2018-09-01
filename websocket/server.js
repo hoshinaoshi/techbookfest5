7f3786ab28f010750068baa06d40aa1b4bc12775
hoshinaoshi <hoshinaoshi@gmail.com>
2018-09-01 11:07:55 +0900

    ログイン昨日追加

--- websocket/server.js
+++ websocket/server.js
@@ -5,16 +5,24 @@ var io = require('socket.io')(server);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
- 
-// chatでメッセージを受信したらクライアント全体にキャスト
-io.on('connection', (socket) => {
-  socket.on('chat', (msg) => {
-    io.emit('chat', msg);
+
+io.on('connection', function(socket){
+
+  var loginUsers = []; //ログインユーザ
+
+  // ログイン処理
+  socket.on('login', function(userInfo){
+    loginUsers[userInfo.userID] = userInfo.userName;
+  });
+
+  // メッセージ送信処理
+  socket.on('message', function(msg){
+    userName = loginUsers[socket.id];
+    io.emit('message', {
+      userName: userName,
+      message: msg
+    });
   });
 });
- 
-server.on('listening', () => {
-  console.log('listening on 3000');
-});
- 
+
 server.listen(3000);
