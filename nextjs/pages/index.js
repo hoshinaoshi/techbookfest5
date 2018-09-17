2534c78a3ce220f91c5e95b577a9433782a4f473
hoshinaoshi <hoshinaoshi@gmail.com>
2018-09-18 06:05:23 +0900

    API呼び出し

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,12 +1,46 @@
+import 'babel-polyfill';
+import React, { Component } from 'react';
+import axios from 'axios'
 import Link from 'next/link' 
 
-const Index = () => (
-  <div>
-    <Link href="/about">
-      <a>About Page</a>
-    </Link>
-    <p>Hello Next.js</p>
-  </div> 
-) 
-
-export default Index
+export default class Index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      statusCode: "",
+      statusText: ""
+    };
+  }
+  async componentDidMount() {
+    const options = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        'content-type': 'application/x-www-form-urlencoded',
+        accept: 'application/json'
+      }
+    };
+    await axios.get('http://mockbin.com/headers', options)
+      .then(response => {
+        this.setState(
+          {
+            statusCode: response.status,
+            statusText: response.statusText
+          }
+        )
+      })
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <Link href="/about">
+            <a>About Page</a>
+          </Link>
+          <p>Hello Next.js</p>
+        </div> 
+        API status code: {this.state.statusCode}<br />
+        API status text: {this.state.statusText}
+      </div>
+    );
+  }
+}
